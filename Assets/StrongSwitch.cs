dd048ab5448e5cc87adf170748c68544732391b6
cgoundory <chetnarose@gmail.com>
2018-05-17 14:20:05 +0400

    Strong Switch

---
+++ Assets/StrongSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrongSwitch : MonoBehaviour {
+
+    GameObject bridge;
+    // Use this for initialization
+    void Start()
+    {
+        bridge = GameObject.Find("StrongBridge");
+        bridge.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Top") || other.gameObject.CompareTag("Bottom"))
+        {
+            bridge.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        
+
+    }
+
+}
