973881dff6f6be723b3e8a47912a87d898ce45d8
vgoundory <goundory1995@gmail.com>
2018-05-25 20:09:26 +0400

    script and implementation of score

---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class score : MonoBehaviour {
+	public Text score1;
+	public int score2;
+
+	void scoreis()
+	{
+		
+		score1.text = "Score:" + score2.ToString ();
+
+	}
+	void Start()
+	{
+		
+		score2 = 0;
+		scoreis();
+
+	
+	}
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+			score2++;
+			
+
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+			score2++;
+
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+			score2++;
+
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+			score2++;
+
+		scoreis();
+	}
+}
+
