cc669e15ba27c30393ef8ce9a4b2797f361a1d7e
cgoundory <chetnarose@gmail.com>
2018-06-03 20:27:05 +0400

    gameover n sart

--- Assets/Scripts/Top.cs
+++ Assets/Scripts/Top.cs
@@ -37,6 +37,41 @@ public class Top : MonoBehaviour {
             Destroy(other.gameObject);
             SceneManager.LoadScene("Level4");
         }
+        if (other.gameObject.CompareTag("Destination4"))
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("Level5");
+        }
+        if (other.gameObject.CompareTag("Destination5"))
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("Level6");
+        }
+        if (other.gameObject.CompareTag("Destination6"))
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("Level7");
+        }
+        if (other.gameObject.CompareTag("Destination7"))
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("Level8");
+        }
+        if (other.gameObject.CompareTag("Destination8"))
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("Level9");
+        }
+        if (other.gameObject.CompareTag("Destination9"))
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("Level10");
+        }
+        if (other.gameObject.CompareTag("Destination10"))
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("gameOver");
+        }
 
     }
 }
