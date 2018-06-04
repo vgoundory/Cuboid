7510aaed56a3158148f9bfb11ee191c96a83194a
vgoundory <goundory1995@gmail.com>
2018-06-04 12:59:41 +0400

    completed levels, options and main menu
    BOOM!

--- Assets/mainmenu.cs
+++ Assets/mainmenu.cs
@@ -13,4 +13,51 @@ public class mainmenu : MonoBehaviour {
     {
         Application.Quit();
     }
+
+
+    //for levels
+    public void PlayGame1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void PlayGame2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void PlayGame3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void PlayGame4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void PlayGame5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void PlayGame6()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void PlayGame7()
+    {
+        SceneManager.LoadScene(7);
+    }
+    public void PlayGame8()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void PlayGame9()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void PlayGame10()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+
+
+
 }
