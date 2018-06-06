fe5e23f5cac8d15bc35d050858dd7bdbf754233e
cgoundory <chetnarose@gmail.com>
2018-06-06 09:38:25 +0400

    timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,36 +5,33 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
 
     public float myTimer = 5;
     private Text timerText;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         timerText = GetComponent<Text>();
 
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         myTimer -= Time.deltaTime;
-        
+
         timerText.text = ("Time left : ") + myTimer.ToString("f0");
-        print(timerText);
         if (myTimer < 0)
         {
             timerText.text = "Time up! You lost!";
-            Wait();
-            SceneManager.LoadScene("gameOver");
-        }
 
-    }
-    IEnumerator Wait()
-    {
-        Debug.Log(2);
-        yield return new WaitForSeconds(3);
-        Debug.Log(3);
+            //SceneManager.LoadScene("gameOver");
+        }
+        print(timerText);
+
     }
 }
