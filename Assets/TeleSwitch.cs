9db2f811f18660ae377aef4177148976fd413ce6
cgoundory <chetnarose@gmail.com>
2018-06-06 13:46:55 +0400

    Teleportation part 1( transforms into 2 cubes)

---
+++ Assets/TeleSwitch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleSwitch : MonoBehaviour {
+
+    GameObject TeleCube1;
+    GameObject TeleCube2;
+    GameObject Player;
+
+
+    // Use this for initialization
+    void Start () {
+
+        TeleCube1 = GameObject.Find("TeleCube1");
+        TeleCube1.SetActive(false);
+        TeleCube2 = GameObject.Find("TeleCube2");
+        TeleCube2.SetActive(false);
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Top") || other.gameObject.CompareTag("Bottom"))
+            {
+            TeleCube1.SetActive(true);
+            TeleCube2.SetActive(true);
+
+            gameObject.SetActive(false);
+            Player.SetActive(false);
+            
+        }
+
+
+    }
+}
