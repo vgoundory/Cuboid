f3ecc23995c5e82343488ecbaf27d90b0969fc5f
vgoundory <goundory1995@gmail.com>
2018-06-06 14:15:39 +0400

    shit

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+    public GameObject pauseButton, pausePanel;
+	// put the rate at which the time passes in the game at 0.
+    public void OnPause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void OnResume()
+    {
+        Time.timeScale = 1;
+    }
+}
