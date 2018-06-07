7a77fdc31b67b573c57e4ea0afe20b95b68155bd
cgoundory <chetnarose@gmail.com>
2018-06-07 19:44:49 +0400

    ...

--- Assets/Scripts/StrongSwitch.cs
+++ Assets/Scripts/StrongSwitch.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class StrongSwitch : MonoBehaviour {
 
-    GameObject bridge;
+    GameObject bridge1;
     GameObject bridge2;
     GameObject bridge3;
     // Use this for initialization
     void Start()
     {
-        bridge = GameObject.FindGameObjectWithTag("StrongBridge");
-        bridge.SetActive(false);
+        bridge1 = GameObject.FindGameObjectWithTag("StrongBridge");
+        bridge1.SetActive(false);
         bridge2 = GameObject.FindGameObjectWithTag("StrongBridge2");
         bridge2.SetActive(false);
         bridge3 = GameObject.FindGameObjectWithTag("StrongBridge3");
@@ -25,7 +25,7 @@ public class StrongSwitch : MonoBehaviour {
     {
         if (other.gameObject.CompareTag("Top") || other.gameObject.CompareTag("Bottom"))
         {
-            bridge.SetActive(true);
+            bridge1.SetActive(true);
             bridge2.SetActive(true);
             bridge3.SetActive(true);
             gameObject.SetActive(false);
