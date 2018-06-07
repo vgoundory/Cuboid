ee6d288388e6d8a078b2770d3bb19fb9ce5328ba
vgoundory <goundory1995@gmail.com>
2018-06-07 21:35:53 +0400

    Revert "Revert "Revert "Highscore done. Working on level editor."""
    This reverts commit e6c038d430c7dac4fd6616bd6ab5f1cecb7ddca1.

--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -5,39 +5,21 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour {
 	public Text score1;
 	public int score2;
-    public Text highscore;
-    
-
- 
 
 	void scoreis()
 	{
 		
 		score1.text = "Score:" + score2.ToString ();
-        PlayerPrefs.SetInt("Highscore", score2);
 
-        if (score2 > PlayerPrefs.GetInt("Highscore", 0))
-        {
-            PlayerPrefs.GetInt("Highscore", score2);
-            highscore.text = score2.ToString();
-        }
-      
-
-    }
+	}
 	void Start()
 	{
-
-
-        highscore.text = PlayerPrefs.GetInt("Highscore",0).ToString();
-
-
+		
 		score2 = 0;
 		scoreis();
-        
 
-
-
-    }
+	
+	}
 	void Update() {
 		if (Input.GetKeyUp (KeyCode.UpArrow))
 			score2++;
@@ -52,21 +34,7 @@ public class Score : MonoBehaviour {
 		else if (Input.GetKeyUp(KeyCode.LeftArrow))
 			score2++;
 
-    
-      
-
-        
-
-        scoreis();
-
-
-
-
-
-
+		scoreis();
 	}
-
-
-
 }
 
