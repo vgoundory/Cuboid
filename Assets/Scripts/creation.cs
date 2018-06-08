a430acbad0b82e248c04e3bfeb7cbd5d8f8ca85c
vgoundory <goundory1995@gmail.com>
2018-06-08 15:21:00 +0400

    player level

--- Assets/Scripts/creation.cs
+++ Assets/Scripts/creation.cs
@@ -2,26 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class creation : MonoBehaviour {
+public class Creation : MonoBehaviour
+{
 
-    public GameObject tile;
+    public GameObject Tile, StrongSwitch, Switch, TeleSwitch, StrongBridge;
+    public string spawnBridge;
 
-    GameObject[] spawn = new GameObject[1];
 
 
-	// Use this for initialization
-	void Start () {
-		for (int i=0; i < spawn.Length; i++)
+    public void CreateTile()
+    {
+        for (int i = 0; i < 1; i++)
         {
-            GameObject spawn = Instantiate(tile);
-            
-
-            spawn.transform.position = new Vector3(i, spawn.transform.position.y);
+            Instantiate(Tile);
+            Debug.Log("Tiles");
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+    public void CreateStrongSwitch()
+    {
+        if (Input.GetButton("spawnStrongSwitch"))
+
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                Instantiate(StrongSwitch);
+                Debug.Log("StrongSwitch");
+
+            }
+        }
+    }
+    public void CreateNormalSwitch()
+    {
+        if (Input.GetButton("spawnNormalSwitch"))
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                Instantiate(Switch);
+                Debug.Log("NormalSwitch");
+
+            }
+        }
+    }
+    public void CreateTeleSwitch()
+    {
+        if (Input.GetButton("spawnTeleSwitch"))
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                Instantiate(TeleSwitch);
+                Debug.Log("TeleSwitch");
+
+            }
+        }
+    }
+    public void CreateBridge()
+    { if(Input.GetButton("spawnBridge"))
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                Instantiate(StrongBridge);
+                Debug.Log("StongBridge");
+
+            }
+        }
+    }
 }
+
+  
